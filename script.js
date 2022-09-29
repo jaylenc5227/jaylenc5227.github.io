800f11b7fcf9673698c878e66e30f7f5f29faca0
jaylenc5227 <114585892+jaylenc5227@users.noreply.github.com>
2022-09-28 20:37:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Jaylen C"; // update the part BETWEEN the "quotes"
+let userName = "jaylenc5227"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
